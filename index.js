1442552cf98b9dcc25e8053fcb0c5bbc2b61accc
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-07-29 12:54:10 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+const names = ["Steve", "Joe", "Abe"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+function writeCards(names, event) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return cards;
+} 
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+wrapGifts(gifts);
+writeCards(names, "birthday");
